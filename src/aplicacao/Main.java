728de5a62a33d7efd5d7f0a36d17cdb59828a62c
Gabriel <gabrielnetworkingg@gmail.com>
2025-10-29 14:26:07 -0300

    primeiro commit com o codigo completo

---
+++ src/aplicacao/Main.java
@@ -0,0 +1,73 @@
+package aplicacao;
+
+import entidades.Funcionario;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos Funcionarios serao registrados?: ");
+        int n = sc.nextInt();
+        int id = 0;
+        String nome ;
+        double salario;
+
+        List<Funcionario> f = new ArrayList<>(); // testando dessa forma
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println();
+            System.out.println("============ " + i + " Funcionario: " + "=============");
+            System.out.print("Digite o Id: ");
+            int getId = sc.nextInt();
+            if (id == getId) {
+                System.out.println("---------------------------");
+                System.out.println("|Erro. Este Id ja existe!!|");
+                System.out.println("---------------------------");
+                i--;
+                continue;
+            } else {
+                id = getId;
+            }
+            System.out.print("Digite o nome do Colaborador: ");
+            sc.nextLine();
+            nome = sc.nextLine();
+            System.out.print("Digite o valor de seu salario: ");
+            salario = sc.nextDouble();
+            Funcionario fun = new Funcionario(nome, id, salario);
+            f.add(fun);
+        }
+
+        Funcionario fu = new Funcionario();
+
+        System.out.print("Deseja aumentar o salario de qual colaborador?: ");
+        int idAumentaSal = sc.nextInt();
+        int porc;
+
+       /* for(int i=0; i<f.size(); i++) {
+            if (f.contains(idAumentaSal)) {
+                System.out.print("Qual a porcentagem de aumento?: ");
+                porc = sc.nextInt();
+                fu.aumentarSalario(porc);
+            } else {
+                System.out.println("---------------------------");
+                System.out.println("|Erro. Este Id ja existe!!|");
+                System.out.println("---------------------------");
+                i--;
+            }
+        }*/
+
+
+        for (Funcionario func : f){
+            System.out.println(func.getId()+ ", " + func.getNome()+ ", " + func.getSalario());
+        }
+
+    sc.close();
+    }
+
+}
---
+++ src/entidades/Funcionario.java
@@ -0,0 +1,47 @@
+package entidades;
+
+import java.util.List;
+
+public class Funcionario {
+    private String nome;
+    private int id;
+    private double salario;
+
+    public void aumentarSalario(double valor) {
+      double porcentagem = valor * salario;
+      salario += salario / 100;
+    }
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, int id, double salario) {
+        this.nome = nome;
+        this.id = id;
+        this.salario = salario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
